3d8ef423ea0355d359a788fca4c270aafbe75c3b
rooc <vadim.grachev@gmail.com>
2023-03-22 12:22:36 +0200

    disclosure

--- src/lib/components/Faq.svelte
+++ src/lib/components/Faq.svelte
@@ -5,6 +5,10 @@
 
   let CurrentDisclosure = "";
 
+  function setDisclosure(id) {
+		console.log(id);
+	}
+
   const faqdata = [
     {
       id: "template",
@@ -38,7 +42,7 @@
         <Disclosure open={item.open}>
           <DisclosureButton
             class="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200"
-            on:click="{() => CurrentDisclosure = item.id}"
+            on:click={() => setDisclosure("Yo")}
           >
             <span>{item.question}</span>
             <ChevronUpIcon
