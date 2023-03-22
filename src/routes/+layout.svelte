431cef264366432a85da9c5a741712fa8da306be
tigerxp <vpupkin@gmail.com>
2023-03-22 15:04:37 +0200

    Renamed CSS file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import PopupWidget from '$lib/components/PopupWidget.svelte';
-  import '../css/tailwind.css';
+  import '../css/app.postcss';
 </script>
 
 <svelte:head>
